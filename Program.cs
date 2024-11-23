eaca278c6e35f67171b5b1eaea7b1e70aeea0c32
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-11-23 12:29:23 +0600

    Create DBContext file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
-
+// Database Connection
+builder.Services.AddDbContext<BillTrackerContext>()
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
