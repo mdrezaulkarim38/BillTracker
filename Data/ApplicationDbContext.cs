d8b73be74fb330907e8e326f43b8323938369f61
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-23 22:27:55 +0600

    Add new thing

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using BillTracker.Models;
+
+namespace BillTracker.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<User> Users { get; set; }
+
+}
