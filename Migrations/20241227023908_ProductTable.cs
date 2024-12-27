96234fabeb6c6e6b74a0b486066e74e1b50212cd
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-12-27 08:42:23 +0600

    migration page

---
+++ Migrations/20241227023908_ProductTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UniqueBillId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PoNo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BillNo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BillDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BillAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Challan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ChallanDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    QuantityReceived = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ItemKey = table.Column<int>(type: "int", nullable: false),
+                    SubmitDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UserId",
+                table: "Products",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
